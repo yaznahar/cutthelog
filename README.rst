a9dec8a935c885a645b881a7a90171581b3b385e
Alexander Larin <znahar@yandex-team.ru>
2023-02-20 13:20:42 +0300

    Start to write documentation

--- README.rst
+++ README.rst
@@ -1,1 +1,61 @@
-**Cutthelog** is Python module and command line utility to read unseen tailing part of text file. Typically it's log one.
+**Cutthelog** is Python module and command-line tool to read unseen tailing part of log files.
+
+The documentation of the module is available on readthedocs.
+The tool description is below. The tool requires only Python 2 or Python 3 interpreter of almost any version (by default using Python 3) and its standard libraries. All code is located in a single file.
+
+Here is an example of its work:
+
+    .. code-block::
+
+        $ echo -e "one\ntwo\nthree" > example
+        $ cutthelog example
+        one
+        two
+        three
+        $ cutthelog example
+        $ echo -e "four\nfive\nsix" >> example
+        $ cutthelog example
+        four
+        five
+        six
+        $ cutthelog example
+        $ echo -e "seven\neight\nnine\nten\neleven\ntwelve" > example
+        $ cutthelog example
+        seven
+        eight
+        nine
+        ten
+        eleven
+        twelve
+
+
+Installation
+------------
+
+From PyPI:
+
+    ::
+        sudo pip install cutthelog
+
+or just download the tool file from Github:
+https://raw.githubusercontent.com/yaznahar/cutthelog/main/cutthelog.py
+make it executable and move to local bin folder
+
+    ::
+        chmod +x cutthelog.py; sudo mv cutthelog.py /usr/local/bin/cutthelog
+
+
+How it works
+------------
+
+The tool stores offset and value of last line of each read file in a cache file. On next reading of that file it jumps to saved offset, read a line and compare its value with the saved one. If they / If don't it reads the file from the begining. At the end the tool update cache values of the file.
+
+By default the cache file locates in user home folder with the name `.cutthelog`. If home folder is unavailable it tries to save cache in current working directory. You can redefine cache path by the option.
+
+Usage
+-----
+
+You can get available option and its meaning by -h/--help option:
+
+    ::
+        $ cutthelog --help
--- cutthelog.py
+++ cutthelog.py
@@ -1,5 +1,6 @@
 #!/usr/bin/env python3
 # -*- coding: utf-8 -*-
+"""Print only unseen tailing part of log file"""
 
 
 from __future__ import print_function
@@ -14,20 +15,17 @@ import tempfile
 VERSION = (1, 0, 0)
 __version__ = '.'.join(map(str, VERSION))
 DEFAULT_POSITION = (0, '')
-DESCRIPTION = ''
+LOG_FORMAT = '%(levelname)s: %(message)s'
+CACHE_DELIMITER = '##'
+CACHE_FILENAME = '.cutthelog'
 HELPS = {
-    'logfile': '',
-    'cache_file': '',
-    'offset': '',
-    'last_line': '',
-    'cache_delimiter': '',
+    'logfile': 'path of the file to read',
+    'cache_file': ('path to the cache file (by default used {} in the home or '
+                   'current folder)'.format(CACHE_FILENAME)),
+    'cache_delimiter': 'delimiter used inside cache record (by default "{}")'.format(CACHE_DELIMITER),
     'verbose': 'enable verbose mode',
     'version': 'print utility version and exit',
 }
-LOG_FORMAT = '%(asctime)s %(levelname)s: %(message)s'
-DATE_FORMAT = '%Y-%m-%d %H:%M:%S'
-CACHE_DELIMITER = '##'
-CACHE_FILENAME = '.cutthelog'
 
 
 class CutthelogError(Exception):
@@ -39,9 +37,18 @@ class CutthelogCacheError(CutthelogError):
 
 
 class CutTheLog:
-    """Class to parse log file"""
+    """An object to read a single text file from cache postition
+
+    Parameters
+    ----------
+    name : |str|
+        An absolute or relative path to the text file.
+    offset : |int|, optional
+        The file position of the last line read on the last interaction
+    last_line : |str|, optional
+        The value of the last line read on the last interaction
+    """
     def __init__(self, name, offset=None, last_line=None):
-        """Initialize object to work with log file"""
         self.name = os.path.normpath(os.path.abspath(name))
         self.offset = None
         self.last_line = None
@@ -56,12 +63,20 @@ class CutTheLog:
         self.last_line = last_line or DEFAULT_POSITION[1]
 
     def __call__(self, offset=None, last_line=None):
-        """Method to run into the with statement"""
+        """Allow to set position inside with statement like
+
+        >>> cutthelog = CutTheLog('/var/log/kern.log')
+        >>> with cutthelog(offset=2605148, last_line='Feb 20 11:22:57 ...') as line_iter:
+        ...     print(*line_iter, sep='')
+        """
         self.set_position(offset, last_line)
         return self
 
     def __enter__(self):
-        """Return iterator of lines of the log file"""
+        """Open the file and check if the position is correct
+
+        If position is wrong reset it to the start of the file
+        Return iterator over unseen lines"""
         fhandler = open(self.name, 'r')
         offset, last_line = self.get_position()
         try:
@@ -75,17 +90,17 @@ class CutTheLog:
         self.fhandler = fhandler
         return iter(self)
 
-    def _is_file_opened(self):
+    def is_file_opened(self):
         return self.fhandler is not None and not self.fhandler.closed
 
     def __exit__(self, exc_type, exc_value, traceback):
-        """Close the log file"""
-        if self._is_file_opened():
+        """Close the file"""
+        if self.is_file_opened():
             self.fhandler.close()
 
     def __iter__(self):
-        """Iterator through lines of the log file"""
-        if not self._is_file_opened():
+        """Iterator through lines of the file"""
+        if not self.is_file_opened():
             return
         offset, last_line = self.get_position()
         offset_change = len(last_line)
@@ -96,7 +111,7 @@ class CutTheLog:
             offset_change = len(line)
 
     def get_eof_position(self):
-        """Return offset and last line without the whole file reading"""
+        """Return offset and value of the last line without reading of the whole file"""
         chunk_size = 512
         last_line_chunks = []
         with open(self.name, 'r') as fhandler:
@@ -121,6 +136,27 @@ class CutTheLog:
         return (self.name + delimiter, delimiter)
 
     def set_position_from_cache(self, cache_file, delimiter=None):
+        """Set file position from cache
+
+        If there is no file record in the cache object position doesn't change
+        A cache record of a file looks like
+        <normalized absolute file path><delimiter><offset value><delimiter><last line value>
+
+        There is no need to parse every cache line so we search only the required one
+        and don't use the csv module
+
+        Parameters
+        ----------
+        cache_file : |str|
+            An path of cache file
+        delimiter : |str|, optional
+            An delimiter using by cache record
+
+        Raises
+        ------
+        `CutthelogCacheError`
+            On failed cache reading or malformed cache record for the file
+        """
         file_prefix, delimiter = self._get_cache_props(delimiter)
         try:
             with open(cache_file, 'r') as fhandler:
@@ -133,16 +169,34 @@ class CutTheLog:
                         try:
                             self.set_position(int(splitted_line[1]), splitted_line[2])
                         except ValueError:
-                            msg = 'Bad offset {0} in line #{1}'.format(splitted_line[1], index)
+                            msg = 'Bad offset {} in line #{}'.format(splitted_line[1], index)
                             raise CutthelogCacheError(msg)
                     else:
-                        msg = 'Malformed cache line #{0}: {1}'.format(index, line.rstrip())
+                        msg = 'Malformed cache line #{}: {}'.format(index, line.rstrip())
                         raise CutthelogCacheError(msg)
         except (EnvironmentError, UnicodeDecodeError) as err:
             raise CutthelogCacheError('Failed to read cache: ' + str(err))
 
     def save_to_cache(self, cache_file, delimiter=None):
-        """Save cache dictionary to cache file"""
+        """Save position of the file to cache
+
+        At first the file record is written to temporary file, then records of other files are
+        appended and finally the temporary file is copyed to the original cache path
+        As a result records of the last read file are stored at the start of the cache so they are
+        found faster on the next run
+
+        Parameters
+        ----------
+        cache_file : |str|
+            An path of cache file
+        delimiter : |str|, optional
+            An delimiter using by cache record
+
+        Raises
+        ------
+        `CutthelogCacheError`
+            On failed cache reading or malformed cache record for the file
+        """
         file_prefix, delimiter = self._get_cache_props(delimiter)
         offset, last_line = self.get_position()
         try:
@@ -161,54 +215,54 @@ class CutTheLog:
 
 
 def argument_parsing():
-    """Parse and return command line arguments"""
-    parser = argparse.ArgumentParser(description=DESCRIPTION)
+    parser = argparse.ArgumentParser(description=__doc__)
     main_group = parser.add_mutually_exclusive_group(required=True)
     main_group.add_argument('logfile', help=HELPS['logfile'], nargs='?')
     main_group.add_argument('-V', '--version', help=HELPS['version'], action='store_true')
     parser.add_argument('-c', '--cache-file', help=HELPS['cache_file'])
     parser.add_argument('--cache-delimiter', help=HELPS['cache_delimiter'],
                         default=CACHE_DELIMITER)
-    parser.add_argument('--offset', help=HELPS['offset'], type=int)
-    parser.add_argument('--last-line', help=HELPS['last_line'])
     parser.add_argument('-v', '--verbose', help=HELPS['verbose'], action='store_true')
     args = parser.parse_args()
     if args.cache_file is None:
-        workdir = os.getcwd() if os.path.exists(CACHE_FILENAME) else os.getenv('HOME')
-        args.cache_file = os.path.join(workdir, CACHE_FILENAME)
+        home = os.getenv('USERPROFILE' if os.name == 'nt' else 'HOME', '/')
+        home_cache = os.path.join(home, CACHE_FILENAME)
+        args.cache_file = home_cache if os.access(home, os.R_OK | os.W_OK) else CACHE_FILENAME
     return args
 
 
 def main():
-    """
-    Main function of command line utility
+    """The cutthelog command line utility
+
+    It uses the basic function `CutTheLog` object. See description in README.rst
     """
     args = argument_parsing()
     if args.version:
         print(__version__)
         return 0
     lvl = logging.DEBUG if args.verbose else logging.WARNING
-    logging.basicConfig(stream=sys.stderr, level=lvl, format=LOG_FORMAT, datefmt=DATE_FORMAT)
+    logging.basicConfig(stream=sys.stderr, level=lvl, format=LOG_FORMAT)
     if not os.path.isfile(args.logfile):
-        logging.error('Log file %s not found', args.logfile)
+        logging.error('File "%s" not found', args.logfile)
         return 2
-    cutthelog = CutTheLog(args.logfile, args.offset, args.last_line)
-    use_cache = args.offset is None or args.last_line is None
-    if use_cache:
-        try:
-            cutthelog.set_position_from_cache(args.cache_file, delimiter=args.cache_delimiter)
-        except CutthelogCacheError as err:
-            logging.error(err)
-            return 4
+    if not os.access(args.logfile, os.R_OK):
+        logging.error('No permission to read "%s"', args.logfile)
+        return 2
+    cutthelog = CutTheLog(args.logfile)
+    try:
+        cutthelog.set_position_from_cache(args.cache_file, delimiter=args.cache_delimiter)
+    except CutthelogCacheError as err:
+        logging.error(err)
+        return 4
     cached_position = cutthelog.get_position()
     try:
         with cutthelog as line_iter:
             for line in line_iter:
                 print(line, end='')
     except (EnvironmentError, UnicodeDecodeError) as err:
-        logging.error('Error reading file: %s', err)
+        logging.error('Failed to read file: %s', err)
         return 3
-    if use_cache and cutthelog.get_position() != cached_position:
+    if cutthelog.get_position() != cached_position:
         try:
             cutthelog.save_to_cache(args.cache_file, delimiter=args.cache_delimiter)
         except CutthelogCacheError as err:
